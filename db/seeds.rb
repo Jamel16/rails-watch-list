6c208f10fcafc4c5fc937ed6b92090c310bcea8b
jamel kaawar <kaawarjamel@gmail.com>
2024-03-08 10:45:39 +0100

    rails watch-list back

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'json'
+require 'open-uri'
+url = URI.parse("http://tmdb.lewagon.com./movie/top_rated?")
+response_serialized = URI.open(url).read
+data = JSON.parse(response_serialized)
+
+data["results"].each do |movie|
+  Movie.create(
+  title: movie['original_title'],
+  poster_url: movie['poster_path'],
+  rating: movie['vote_average'],
+  overview: movie['overview'],
+  )
+end
